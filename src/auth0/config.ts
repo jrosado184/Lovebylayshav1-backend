440f0088e4c0462e9a224bd9878b0a05a0594958
Javier Rosado <javy@Javiers-MacBook-Pro.local>
2023-09-23 21:07:38 -0400

    update auth0 config with heroku url

--- src/auth0/config.ts
+++ src/auth0/config.ts
@@ -2,7 +2,7 @@ export const config = {
     authRequired: false,
     auth0Logout: true,
     secret: String(process.env.SECRET),
-    baseURL: 'https://localhost:9000',
+    baseURL: 'https://lovebylaysha-86af4028a14e.herokuapp.com',
     clientID: 'ekVgETOdR2XWhAYnT9sVFloWZDx2dfs2',
     issuerBaseURL: 'https://dev-atfwayiwbxrtxo0y.us.auth0.com'
   };
